52cde103320f66a4d1b9bfeea722e5251d2f2944
cobi-dev0615 <stanislavsokolovskyi44@gmail.com>
2026-02-21 21:14:19 +0000

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const apiUrl = env.VITE_API_URL || 'http://127.0.0.1:8001'
+  const apiUrl = env.VITE_API_URL || 'http://127.0.0.1:8081'
 
   return {
     plugins: [react()],
